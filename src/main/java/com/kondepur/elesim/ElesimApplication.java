614bc32b237c942aa299ee3e8e6838f8125ce44a
Abhinav Kondaji <akondaji@MBP.local>
2024-12-28 17:18:34 -0800

    init

---
+++ src/main/java/com/kondepur/elesim/ElesimApplication.java
@@ -0,0 +1,13 @@
+package com.kondepur.elesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElesimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElesimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kondepur/elesim/ElesimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kondepur.elesim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElesimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
